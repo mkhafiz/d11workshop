d6a2399ac6d6811a766b131925e04483c3a18086
kayu <your_email>
2022-07-18 16:38:20 +0800

    Added src/main file

--- src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
+++ src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.d11workshop;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11workshopApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(D11workshopApplication.class, args);
-		SpringApplication 
+		// SpringApplication.run(D11workshopApplication.class, args);
+		SpringApplication app = new SpringApplication(D11workshopApplication.class);
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
-
 }

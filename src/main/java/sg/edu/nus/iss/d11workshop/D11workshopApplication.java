98da55487c922a9bb6a0e0871e0003f0288baf31
kayu <your_email>
2022-07-18 16:22:50 +0800

    Add system properties

--- src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
+++ src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11workshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11workshopApplication.class, args);
+		//SpringApplication.run(D11workshopApplication.class, args);
+		SpringApplication 
 	}
 
 }
